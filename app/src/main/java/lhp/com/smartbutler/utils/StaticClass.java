c47b139974bc2a4f781b60e31eabcac5b14de5ec
lhp9916 <lhp9916@gmail.com>
2017-07-08 23:21:01 +0800

    项目架构部署

---
+++ app/src/main/java/lhp/com/smartbutler/utils/StaticClass.java
@@ -0,0 +1,8 @@
+package lhp.com.smartbutler.utils;
+
+/**
+ * 存放数据/常量
+ */
+
+public class StaticClass {
+}
---
+++ app/src/main/java/lhp/com/smartbutler/utils/UtilTools.java
@@ -0,0 +1,8 @@
+package lhp.com.smartbutler.utils;
+
+/**
+ * 统一工具类
+ */
+
+public class UtilTools {
+}
