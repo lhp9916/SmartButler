833c2e1307310f23547e5f476dfe9fece70c3174
lhp9916 <lhp9916@gmail.com>
2017-07-09 00:55:17 +0800

    TabLayout逻辑实现

--- app/src/main/java/lhp/com/smartbutler/MainActivity.java
+++ app/src/main/java/lhp/com/smartbutler/MainActivity.java
@@ -1,11 +1,14 @@
 package lhp.com.smartbutler;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,13 +19,16 @@ import lhp.com.smartbutler.fragment.ButlerFragment;
 import lhp.com.smartbutler.fragment.GirlFragment;
 import lhp.com.smartbutler.fragment.UserFragment;
 import lhp.com.smartbutler.fragment.WechatFragment;
+import lhp.com.smartbutler.ui.SettingActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @InjectView(R.id.mTabLayout)
     TabLayout mTabLayout;
     @InjectView(R.id.mViewPager)
     ViewPager mViewPager;
+    @InjectView(R.id.fab_setting)
+    FloatingActionButton fabSetting;
 
     //title
     private List<String> mTitle;
@@ -42,8 +48,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
+        fabSetting.setOnClickListener(this);
+        //默认隐藏
+        fabSetting.setVisibility(View.GONE);
+
         //预加载
         mViewPager.setOffscreenPageLimit(mFragment.size());
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position == 0) {
+                    fabSetting.setVisibility(View.GONE);
+                } else {
+                    fabSetting.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
         //设置适配器
         mViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
@@ -70,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void initData() {
         mTitle = new ArrayList<>();
-        mTitle.add("管家服务");
+        mTitle.add("服务管家");
         mTitle.add("微信精选");
         mTitle.add("美女社区");
         mTitle.add("个人中心");
@@ -80,5 +111,14 @@ public class MainActivity extends AppCompatActivity {
         mFragment.add(new WechatFragment());
         mFragment.add(new GirlFragment());
         mFragment.add(new UserFragment());
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.fab_setting:
+                startActivity(new Intent(this, SettingActivity.class));
+                break;
+        }
     }
 }
---
+++ app/src/main/java/lhp/com/smartbutler/ui/SettingActivity.java
@@ -0,0 +1,15 @@
+package lhp.com.smartbutler.ui;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import lhp.com.smartbutler.R;
+
+public class SettingActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
