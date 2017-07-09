e4928c890e2bc7b97ead5ad1bd82c7facf9cb046
lhp9916 <lhp9916@gmail.com>
2017-07-09 14:27:19 +0800

    集成 Bugly

--- app/src/main/java/lhp/com/smartbutler/application/BaseApplication.java
+++ app/src/main/java/lhp/com/smartbutler/application/BaseApplication.java
@@ -2,9 +2,15 @@ package lhp.com.smartbutler.application;
 
 import android.app.Application;
 
+import com.tencent.bugly.crashreport.CrashReport;
+
+import lhp.com.smartbutler.utils.SecretKey;
+
 public class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        //初始化Bugly
+        CrashReport.initCrashReport(getApplicationContext(), SecretKey.BUGLY_APP_ID, true);
     }
 }
