04d812e5a3313764416d60e30fed8594fe30e932
lhp9916 <lhp9916@gmail.com>
2017-07-09 11:55:46 +0800

    引导页逻辑

--- app/src/main/java/lhp/com/smartbutler/ui/GuideActivity.java
+++ app/src/main/java/lhp/com/smartbutler/ui/GuideActivity.java
@@ -1,9 +1,21 @@
 package lhp.com.smartbutler.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import lhp.com.smartbutler.MainActivity;
 import lhp.com.smartbutler.R;
 
 /**
@@ -11,10 +23,129 @@ import lhp.com.smartbutler.R;
  * description: 引导页
  */
 
-public class GuideActivity extends AppCompatActivity {
+public class GuideActivity extends AppCompatActivity implements View.OnClickListener {
+    @InjectView(R.id.mViewPager)
+    ViewPager mViewPager;
+    @InjectView(R.id.point1)
+    ImageView point1;
+    @InjectView(R.id.point2)
+    ImageView point2;
+    @InjectView(R.id.point3)
+    ImageView point3;
+    @InjectView(R.id.iv_back)
+    ImageView ivBack;
+    private List<View> mList = new ArrayList<>();
+    private View view1, view2, view3;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+        ButterKnife.inject(this);
+        initView();
+    }
+
+    private void initView() {
+        setPointImg(true, false, false);
+        ivBack.setOnClickListener(this);
+
+        view1 = View.inflate(this, R.layout.pager_item_one, null);
+        view2 = View.inflate(this, R.layout.pager_item_tow, null);
+        view3 = View.inflate(this, R.layout.pager_item_three, null);
+
+        view3.findViewById(R.id.btn_start).setOnClickListener(this);
+
+        mList.add(view1);
+        mList.add(view2);
+        mList.add(view3);
+
+        //设置适配器
+        mViewPager.setAdapter(new GuideAdapter());
+
+        //监听ViewPager滑动
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            //pager切换
+            @Override
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                        setPointImg(true, false, false);
+                        ivBack.setVisibility(View.VISIBLE);
+                        break;
+                    case 1:
+                        setPointImg(false, true, false);
+                        ivBack.setVisibility(View.VISIBLE);
+                        break;
+                    case 2:
+                        setPointImg(false, false, true);
+                        ivBack.setVisibility(View.GONE);
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    //设置小圆点的选中效果
+    private void setPointImg(boolean isCheck1, boolean isCheck2, boolean isCheck3) {
+        if (isCheck1) {
+            point1.setBackgroundResource(R.drawable.point_on);
+        } else {
+            point1.setBackgroundResource(R.drawable.point_off);
+        }
+        if (isCheck2) {
+            point2.setBackgroundResource(R.drawable.point_on);
+        } else {
+            point2.setBackgroundResource(R.drawable.point_off);
+        }
+        if (isCheck3) {
+            point3.setBackgroundResource(R.drawable.point_on);
+        } else {
+            point3.setBackgroundResource(R.drawable.point_off);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_start:
+            case R.id.iv_back:
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+                break;
+        }
+    }
+
+    private class GuideAdapter extends PagerAdapter {
+        @Override
+        public int getCount() {
+            return mList.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            ((ViewPager) container).addView(mList.get(position));
+            return mList.get(position);
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            ((ViewPager) container).removeView(mList.get(position));
+            //super.destroyItem(container, position, object);
+        }
     }
 }
