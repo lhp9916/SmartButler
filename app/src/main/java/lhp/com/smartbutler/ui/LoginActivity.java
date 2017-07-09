30b26ddeccf8a3dcc417fe47fdeb6b517df438dc
lhp9916 <lhp9916@gmail.com>
2017-07-09 23:12:49 +0800

    用户登录

--- app/src/main/java/lhp/com/smartbutler/ui/LoginActivity.java
+++ app/src/main/java/lhp/com/smartbutler/ui/LoginActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -17,6 +18,7 @@ import cn.bmob.v3.listener.SaveListener;
 import lhp.com.smartbutler.MainActivity;
 import lhp.com.smartbutler.R;
 import lhp.com.smartbutler.entity.MyUser;
+import lhp.com.smartbutler.utils.ShareUtils;
 
 /**
  * Created by lhp on 2017/7/9.
@@ -32,6 +34,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     EditText etPassword;
     @InjectView(R.id.btn_login)
     Button btnLogin;
+    @InjectView(R.id.cb_keepPassword)
+    CheckBox cbKeepPassword;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,6 +48,14 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private void initView() {
         btnLogin.setOnClickListener(this);
         btnRegister.setOnClickListener(this);
+
+        //设置选中状态
+        boolean isChecked = ShareUtils.getBoolean(this, "keeppass", false);
+        cbKeepPassword.setChecked(isChecked);
+        if (isChecked) {
+            etUser.setText(ShareUtils.getString(this, "name", ""));
+            etPassword.setText(ShareUtils.getString(this, "password", ""));
+        }
     }
 
     @Override
@@ -57,15 +69,20 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 String password = etPassword.getText().toString().trim();
                 if (!TextUtils.isEmpty(name) & !TextUtils.isEmpty(password)) {
                     //登录
-                    MyUser user = new MyUser();
+                    final MyUser user = new MyUser();
                     user.setUsername(name);
                     user.setPassword(password);
                     user.login(new SaveListener<MyUser>() {
                         @Override
                         public void done(MyUser myUser, BmobException e) {
                             if (e == null) {
-                                Toast.makeText(LoginActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                //判断邮箱是否验证
+                                if (user.getEmailVerified()) {
+                                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                    finish();
+                                } else {
+                                    Toast.makeText(LoginActivity.this, "请前往邮箱验证", Toast.LENGTH_SHORT).show();
+                                }
                             } else {
                                 Toast.makeText(LoginActivity.this, "登录失败 " + e.toString(), Toast.LENGTH_SHORT).show();
                             }
@@ -75,6 +92,22 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                     Toast.makeText(this, "用户名或密码不能为空", Toast.LENGTH_SHORT).show();
                 }
                 break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //保存状态
+        ShareUtils.putBoolean(this, "keeppass", cbKeepPassword.isChecked());
+
+        //是否记住密码
+        if (cbKeepPassword.isChecked()) {
+            ShareUtils.putString(this, "name", etUser.getText().toString().trim());
+            ShareUtils.putString(this, "password", etPassword.getText().toString().trim());
+        } else {
+            ShareUtils.deleteShare(this, "name");
+            ShareUtils.deleteShare(this, "password");
         }
     }
 }
