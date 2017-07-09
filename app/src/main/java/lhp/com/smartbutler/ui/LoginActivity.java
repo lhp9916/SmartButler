8a8d96dce457cf73c96b32e44685fc18531caab9
lhp9916 <lhp9916@gmail.com>
2017-07-09 17:35:02 +0800

    集合 Bmob，实现用户登录

--- app/src/main/java/lhp/com/smartbutler/ui/LoginActivity.java
+++ app/src/main/java/lhp/com/smartbutler/ui/LoginActivity.java
@@ -4,12 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.SaveListener;
+import lhp.com.smartbutler.MainActivity;
 import lhp.com.smartbutler.R;
+import lhp.com.smartbutler.entity.MyUser;
 
 /**
  * Created by lhp on 2017/7/9.
@@ -19,6 +26,12 @@ import lhp.com.smartbutler.R;
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     @InjectView(R.id.btn_register)
     Button btnRegister;
+    @InjectView(R.id.et_user)
+    EditText etUser;
+    @InjectView(R.id.et_password)
+    EditText etPassword;
+    @InjectView(R.id.btn_login)
+    Button btnLogin;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,6 +42,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     }
 
     private void initView() {
+        btnLogin.setOnClickListener(this);
         btnRegister.setOnClickListener(this);
     }
 
@@ -37,6 +51,29 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         switch (view.getId()) {
             case R.id.btn_register:
                 startActivity(new Intent(this, RegisteredActivity.class));
+                break;
+            case R.id.btn_login:
+                String name = etUser.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+                if (!TextUtils.isEmpty(name) & !TextUtils.isEmpty(password)) {
+                    //登录
+                    MyUser user = new MyUser();
+                    user.setUsername(name);
+                    user.setPassword(password);
+                    user.login(new SaveListener<MyUser>() {
+                        @Override
+                        public void done(MyUser myUser, BmobException e) {
+                            if (e == null) {
+                                Toast.makeText(LoginActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            } else {
+                                Toast.makeText(LoginActivity.this, "登录失败 " + e.toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(this, "用户名或密码不能为空", Toast.LENGTH_SHORT).show();
+                }
                 break;
         }
     }
